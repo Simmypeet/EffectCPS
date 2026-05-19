1610bbc30bd88d4378ebb2d014024c90b5347492
Simmypeet <simmypeet@outlook.com>
2026-05-19 10:47:02 +0200

    feat: LowerExpr

---
+++ src/main/scala/LowerExpr.scala
@@ -0,0 +1,14 @@
+package lowerExpr
+
+
+enum LowerExpr {
+    case Num(n: Int)
+    case String(s: String)
+    case Add(e1: LowerExpr, e2: LowerExpr)
+    case Lambda(params: List[String], body: LowerExpr)
+    case Var(name: String)
+    case App(func: LowerExpr, args: List[LowerExpr])
+    case Let(name: String, value: LowerExpr, body: LowerExpr)
+    case Array(elements: List[LowerExpr])
+    case Index(array: LowerExpr, index: LowerExpr)
+}
