cffaeb0d725e616eb57aca5e98ff1e0845dba1a3
Simmypeet <simmypeet@outlook.com>
2026-05-19 10:46:30 +0200

    chore: add basic syntax annotation

--- src/main/scala/Expr.scala
+++ src/main/scala/Expr.scala
@@ -3,16 +3,42 @@ package expr
 enum Expr {
     case Num(n: Int)
     case String(s: String)
+
+    /// <add> ::= <expr> '+' <expr>
     case Add(e1: Expr, e2: Expr)
+
+    /// <lambda> ::= '\' <ident>+ '.' <expr>
     case Lambda(params: List[String], body: Expr)
+
+    /// <parens> ::= '(' <expr> ')'
+    case Parens(expr: Expr)
+
+    /// <var> ::= <ident>
     case Var(name: String)
+
+    /// <app> ::= <expr> '(' <expr>+ ')'
     case App(func: Expr, args: List[Expr])
-    case Let(name: String, value: Expr, body: Expr)
-    case Array(elements: List[Expr])
-    case Do(label: String, body: Expr, handler: Handler)
+
+    /// <let> ::= ('let' <ident> '=')? <expr> ';' <expr>
+    case Let(name: Option[String], value: Expr, body: Expr)
+
+    /// <tuple> ::= '[' <expr> (',' <expr>)* ']'
+    case Tuple(elements: List[Expr])
+
+    /// <index> ::= <expr> '[' <expr> ']'
+    case Index(array: Expr, index: Expr)
+
+    /// <handle> ::= 'handle' <expr> 'with' <handler>
+    case Handle(body: Expr, handler: Handler)
+
+    /// <do> ::= 'do' <ident> <expr>
+    case Do(label: String, arg: Expr)
 }
 
+/// <returnClause> ::= 'return' <ident> '->' <expr>
 case class ReturnClause(param: String, body: Expr)
+
+/// <operationClause> ::= 'case' <ident> '(' <ident>, <ident> ')' '->' <expr>
 case class OperationClause(
     label: String,
     param: String,
@@ -20,6 +46,7 @@ case class OperationClause(
     body: Expr
 )
 
+/// <handler> ::= '{' <returnClause> <operationClause>* '}'
 case class Handler(
     returnClause: ReturnClause,
     operationClause: List[OperationClause]
