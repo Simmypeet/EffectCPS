f776b3b9d80064e35c2a74338172b1fee9ae01e4
Simmypeet <simmypeet@outlook.com>
2026-05-19 10:48:47 +0200

    feat: make let optional over parameter name

--- src/main/scala/LowerExpr.scala
+++ src/main/scala/LowerExpr.scala
@@ -8,7 +8,7 @@ enum LowerExpr {
     case Lambda(params: List[String], body: LowerExpr)
     case Var(name: String)
     case App(func: LowerExpr, args: List[LowerExpr])
-    case Let(name: String, value: LowerExpr, body: LowerExpr)
+    case Let(name: Option[String], value: LowerExpr, body: LowerExpr)
     case Array(elements: List[LowerExpr])
     case Index(array: LowerExpr, index: LowerExpr)
 }
