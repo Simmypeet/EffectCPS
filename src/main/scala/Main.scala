9747b0dc0dc1c09e910a766a6575d3f6b9042f5f
Simmypeet <simmypeet@outlook.com>
2026-05-20 09:46:14 +0200

    refactor: given main lowering strategy

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,4 +1,8 @@
+import expr.LoweringStrategy
+import expr.CuryCpsLoweringStrategy
 @main
 def emitDrunkTossJs(): Unit =
+  given LoweringStrategy = CuryCpsLoweringStrategy
+
   val outputPath = Examples.writeDrunkTossJavaScript()
   println(s"Wrote $outputPath")
